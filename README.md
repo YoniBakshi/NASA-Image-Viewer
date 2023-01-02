122f215eb21238f9dea9597440f21960601b6051
Yoni <Yoni@DESKTOP-E5PE3BL>
2023-01-02 23:46:08 +0200

    final

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=9537662&assignment_repo_type=AssignmentRepo)
 
-# Ex05 REST API
+# Ex-05 REST API
 
 # Authors:
 Yehonatan Bakshi , ID : 308077668 , Email : yehonatanba@edu.hac.ac.il
@@ -8,31 +7,33 @@ Yehonatan Bakshi , ID : 308077668 , Email : yehonatanba@edu.hac.ac.il
 Hila Saadon , ID : 208405217 , Email : hilasaa@edu.hac.ac.il
 
 README :
-- In this Express project we've created a social website of NASA's media.
-- Interacting with NASA's data by using GET and POST.
-- At first, the user needs to login by enter a valid username - contains letters a-z, A-Z, 0-9 ONLY.
+- In this Express project we've created a social website using NASA's media.
+- Interacting with NASA's data by using REST API.
+- At first, the user needs to login by enter a valid username :
+  - Must contain letters and digits : a-z, A-Z, 0-9 ONLY.
   - No spaces are allowed.
-  - Can contain up to 24 letters/digits. 
-- After login successfully, a feed page of NASA's daily picture will be loaded (default up to date) plus - previous 2 days
-  - Each picture is styled by a card and detailed : 
+  - Must be between 1-24 letters/digits.
+- After login successfully, a feed page of NASA's daily picture will be loaded (default up to date) plus - it's previous 2 days.
+  - Each media is styled by a card and detailed :
+    - Media = Image/Video
     - Title
     - Date
     - Explanation
     - Copyrights
-  - plus, each has a "Comments" button attached which opens a Modal with the picture in a bigger view, explanation and comments.
-- User can pick a date and feed will be shown loaded by the results of NASA's data 
-
-
-- Comments : 
-  - Username of the writer is attached.
-  - Submit comment by press "Enter".
+  - plus, each media has a "Comments" button attached which opens a Modal with the picture in a bigger view, explanation and comments.
+- User can pick a date and it will be loaded on feed page by the results of NASA's data.
+- By clicking on "Comments" button, a modal will be open with this specific media, title, date, explanation and comments.
+- Comments :
+  - Comments area is located to the right of Modal.
+  - Published comments (if exists) will be shown with it's writer's username.
+  - Option to submit a comment by press "Enter".
   - Comment must be between 1-128 letters.
   - Only the writer of the comment has the option to delete his comment.
-  - 
 
 
 Notes :
 - Default feed date is up to date.
-- Using infinite scrolling, loading a serie of 3 more pictures.
+- Date input can be change by click and pick in the calendar or use keyboard.
+- Using infinite scrolling, each loading is a serie of 3 more pictures.
 - Read more/less button in explanations.
 - display almost fully 3 pictures at a time. (pictures are in a reasonable size)
--- public/javascripts/funcs.js
+++ public/javascripts/funcs.js
@@ -2,18 +2,18 @@
 
 (function () {
     const APIURL = 'https://api.nasa.gov/planetary/apod';
-    const APIKEY = 'JbfLgvG7Tu218jEiWKZH4jMOrscQvu1mDJoctND3';
+    const APIKEY = '4Y3Sd7QzaQF3QK1eomVPaMgysACy1D2FKIgFfyeh';
 
     document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("uname").addEventListener("keyup", validatorName)
         //get a picked date
-        document.getElementById("currentDate").addEventListener("change", (event)=>{
+        document.getElementById("currentDate").addEventListener("change", (event) => {
             //clear image card
             document.getElementById("imgCard").innerHTML = "";
             let currDate = new Date(event.target["value"])
             date.updateDate(currDate)
         })
-        //get at first the 3 last days pictures by default when login
+        // At first, default date is set up to date - display NASA's daily media of last 3 days.
         document.getElementById("loginBox").addEventListener("submit", (elm) => {
             elm.preventDefault()
             if (validatorName()) {
@@ -22,24 +22,24 @@
             }
         });
 
-        //Infinite scroll - loading 3 pictures every time
+        // Infinite scroll - loading 3 pictures every time
         document.addEventListener("scroll", () => {
-            if ( window.innerHeight + window.scrollY >= document.documentElement.scrollHeight-1) {
+            if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1) {
                 let currDate = new Date(date.getNewDate().start)
                 currDate.setDate(currDate.getDate() - 1);
                 date.updateDate(currDate)
             }
         })
-        //check written comment if valid (between 1 - 128 characters) and add it to comment's array.
+        // Validate written comment (between 1 - 128 characters), if valid - add it to comment's array.
         document.getElementById("commentPost").addEventListener("keypress", (elm) => {
-            if(elm.key === 'Enter' && validatorInput(elm)) {
+            if (elm.key === 'Enter' && validatorInput(elm)) {
                 postData(elm)
                 document.getElementById("user-input").value = "";
             }
         });
         // Update comments
-        document.getElementById('imgModal').addEventListener("keypress", (elm) =>{
-            if(elm.key === 'Enter')
+        document.getElementById('imgModal').addEventListener("keypress", (elm) => {
+            if (elm.key === 'Enter')
                 getDataPostedComments(elm)
         });
     });
@@ -56,10 +56,11 @@
          * @param prior2dates = according to 'pickedDate', contain the end date which is the date of 2 previous days
          */
         let pickedDate, prior2Dates, clickedImg;
+
         function initDate() {
-            creatToggle("header-container");      //Display pick a date box
-            creatToggle("page");            //Display feed page
-            creatToggle("loginBox");        //Hide login box
+            creatToggle("header-container");      //Display title container with pick a date box
+            creatToggle("page");                  //Display feed page
+            creatToggle("loginBox");              //Hide login box
             pickedDate = new Date()
             prior2Dates = new Date()
             document.getElementById('currentDate').valueAsDate = pickedDate
@@ -71,7 +72,7 @@
 
         /**
          * Module parameters.
-         * NOTE : to match the requested format of timeline - we'll switch the meanings :
+         * NOTE : to match the requested format of timeline - we'll switch the meanings : (returns these values : )
          * 'pickedDate' named as 'end' date (since it's the latest date) and in relation to it,
          * 'prior2Dates' named as 'start' date (since it's the furthest date)
          */
@@ -90,6 +91,7 @@
         function setCommentDate(date) {
             clickedImg = date
         }
+
         /**
          * Update values of the 2 variables which saves the dates of 3 days range.
          * @param curr = current date, set according to it (Needs to calculate the range to save 2 previous days.)
@@ -113,6 +115,7 @@
     }();
 
     /**
+     *
      * GET request - send to NASA to get the data of requested dates.
      * Error message = same as NASA's
      */
@@ -121,21 +124,19 @@
         if (!isValidDate(date.getNewDate().end) || !isValidDate(date.getNewDate().start))
             document.getElementById("main-container").innerHTML = `<h1 class="text-bg-light">Error: Date is not defined</h1>`;
 
-
         fetch(`${APIURL}?api_key=${APIKEY}&start_date=${date.getNewDate().start}&end_date=${date.getNewDate().end}`)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                if(data.code === 400)
+                if (data.code === 400)
                     throw new Error(`code: ${data.code} Msg: ${data.msg}`)
                 create3ImgSerie(data)
             })
-            .catch(function (error) {
+            .catch(function (error) {                       // NASA's error message
                 document.getElementById("main-container").innerHTML = `<h1 class="text-bg-light">${error}</h1>`
             });
     }
-
 
     /**
      * Create 3 card images in each loading.
@@ -147,37 +148,48 @@
             document.getElementById("imgCard").innerHTML += createHtmlImgInfo(item);
         });
         // AddEventListener to each "Comments" button. (= in every card image)
-        document.querySelectorAll(".comments").forEach((item) =>{
+        document.querySelectorAll(".comments").forEach((item) => {
             item.addEventListener("click", getDataImgModal)
         })
-        readMoreLessBtn()       // If needed - AddEventListener to "Read more/less" explanation field.
+        readMoreLessBtn()       // If needed (in condition to it's length) - AddEventListener to "Read more/less" explanation field.
     }
 
+    /**
+     * Function for Read more & Read less according to signs
+     */
     const readMoreLessBtn = () => {
-        document.querySelectorAll(".readMore").forEach((item) =>{
-            item.addEventListener("click", (elm)=>{
-                setReadBtn(elm.target["id"],'m','l','e')
+        document.querySelectorAll(".readMore").forEach((item) => {
+            item.addEventListener("click", (elm) => {
+                setReadBtn(elm.target["id"], 'm', 'l', 'e')
             })
         })
 
-        document.querySelectorAll(".readLess").forEach((item) =>{
-            item.addEventListener("click", (elm)=>{
-                setReadBtn(elm.target["id"],'l','m','e')
+        document.querySelectorAll(".readLess").forEach((item) => {
+            item.addEventListener("click", (elm) => {
+                setReadBtn(elm.target["id"], 'l', 'm', 'e')
             })
         })
     }
 
-    const setReadBtn = (id,c1,c2,c3) =>{
+    /**
+     * Modular function to toggle when needed
+     * @param id = Read More or Read Less
+     * @param c1 = to change
+     * @param c2 = change to
+     * @param c3 = change to
+     */
+    const setReadBtn = (id, c1, c2, c3) => {
         creatToggle(`${id}`)
-        creatToggle(`${id.replace(c1,c2)}`)
-        creatToggle(`${id.replace(c1,c3)}`)
+        creatToggle(`${id.replace(c1, c2)}`)
+        creatToggle(`${id.replace(c1, c3)}`)
     }
 
-    // @param 'intrevalId' uses to announce the timer every 15 seconds
+    // @param 'intrevalId' = uses to announce the timer every 15 seconds
     let intrevalId = 0;
     /**
-     *
-     * @param elem
+     * REST API
+     * Get data from NASA about clicked image
+     * @param elem = Media's "Comments" button which was clicked
      */
     const getDataImgModal = (elem) => {
         document.getElementById("comments-data").innerHTML = '';
@@ -195,58 +207,81 @@
             })
             .then(function (data) {
                 openModal(data);
-                getDataPostedComments();
-                if (intrevalId)
-                    clearInterval(intrevalId);
-                intrevalId = setInterval(getDataPostedComments, 15000);
+                getDataPostedComments();           // Get Published comments of specific clicked media
+                if (intrevalId)                    // Timer of 15 seconds, clear and start over when reach to 15 seconds
+                    clearInterval(intrevalId);     // Initialize timer
+                intrevalId = setInterval(getDataPostedComments, 15000);  // Refresh comments every 15 seconds
             })
-            .catch(function (error) {
+            .catch(function (error) {              //
                 document.getElementById("main-container").innerHTML = `<h1 class="text-bg-light">${error}</h1>`;
             });
     };
 
-    const openModal = (data) =>{
+    /**
+     * Open Modal of clicked media's button - display media :
+     * explanation, date, published comments & option to add comment
+     * @param data = received data (from NASA) of clicked media's button
+     */
+    const openModal = (data) => {
+        date.commentDate(data.date);
         let mediaElem = getMediaType(data, "");
         document.getElementById("inputSizeError").classList.add('d-none');
         document.getElementById("media-type").innerHTML = mediaElem;
         document.getElementById("imgModalLabel").innerHTML = data.title;
         document.getElementById("modal-date").innerHTML = fixDate(data.date);
-        date.commentDate(data.date);
         document.getElementById("data-section").innerHTML = data['explanation'];
     }
 
     /**
-     * Loop
-     * @param currData = data of current date which it loads at the moment
-     * @returns {string} string HTML modify card images according to received data
+     * Loop to get data of 3 media according to picked date and 2 previous days.
+     * startExpla = First part of explanation for button Read more/less.
+     * endExpla =  Last part of explanation for button Read more/less.
+     * mediaElem = Get type of clicked media.
+     * @param currData = data of current date which it loads at the moment.
+     * @returns {string} string HTML modify card images according to received.
      */
     const createHtmlImgInfo = (currData) => {
-        let start, end, moreBtn, mediaElem, cardImg;
-        if(currData['explanation'].length > 100) {
-            start = currData['explanation'].split(' ').slice(0,40).join(' ')
-            end = currData['explanation'].split(' ').slice(40,).join(' ')
-            moreBtn = `${start}<a class="btn btn-link readMore" id="b${currData.date}m">...Read more</a> <span class="d-none" id="b${currData.date}e"> ${end}</span>
-                                   <a class="btn btn-link readLess d-none" id="b${currData.date}l">Read less</a>`
+        let startExpla, endExpla, moreBtn, mediaElem, cardImg;
+        if (currData['explanation'].length > 100) {
+            startExpla = currData['explanation'].split(' ').slice(0, 40).join(' ')
+            endExpla = currData['explanation'].split(' ').slice(40,).join(' ')
+            moreBtn = `${startExpla}<a class="btn btn-link readMore" id="b${currData.date}m">...Read more</a>
+                               <span class="d-none" id="b${currData.date}e"> ${endExpla}</span>
+                               <a class="btn btn-link readLess d-none" id="b${currData.date}l">Read less</a>`
         } else
             moreBtn = `${currData['explanation']}`
 
         mediaElem = getMediaType(currData, "max-height: 300px; min-height: 300px;")
-
+        //cop = copyrights
         let cop = 'Unknown'
-        if(currData['copyright'])
+        if (currData['copyright'])
             cop = currData['copyright'];
 
         cardImg = createHtmlCard(currData, moreBtn, mediaElem, cop)
         return cardImg;
     }
 
+    /**
+     * Get type of specific media - if it's an image or an iframe video
+     * @param currData = data of specific day
+     * @param currStyle = modified
+     * @returns {string} = image or iframe
+     */
     const getMediaType = (currData, currStyle) => {
         return (currData['media_type'] === 'image') ?
             `<img src=${currData.url} class="card-img-top " style="${currStyle}" alt="#">` :
             `<iframe src=${currData.url} class="card-img-top " style="${currStyle}"></iframe>`;
     }
 
-    const createHtmlCard = (currData, moreBtn, mediaElem, cop)=>{
+    /**
+     * Create card of media, modify it and return the element HTML string.
+     * @param currData = data of specific day
+     * @param moreBtn = Read More or Read Less button
+     * @param mediaElem = media's type
+     * @param cop = copyrights
+     * @returns html string
+     */
+    const createHtmlCard = (currData, moreBtn, mediaElem, cop) => {
         return `<div class="card mb-3" >
                 <div class="row g-0 ">
                     <div class="col-12 col-md-3">${mediaElem}</div>
@@ -265,10 +300,20 @@
                 </div>
             </div>`
     }
-    const creatToggle = (elm) =>{
-        document.getElementById(elm).classList.toggle("d-none")
+
+    /**
+     * Toggle element
+     * @param elmId = id of element to toggle
+     */
+    const creatToggle = (elmId) => {
+        document.getElementById(elmId).classList.toggle("d-none")
     }
-    // POST REQUEST
+
+    /**
+     * POST REQUEST
+     * Create post method and send data of written comment to server
+     * @param event = Submit comment
+     */
     function postData(event) {
         event.preventDefault();
         // Validation: Make sure the event target has a value property
@@ -283,47 +328,60 @@
                 "currComment": event.target.value,
                 "id": date.getNewDate().date
             })
-        }).then(function(response) {
+        }).then(function (response) {
             // If the response is not in the 200 range, it's an error
             if (!response.ok)
                 throw new Error(response.statusText);
             return response.json();
-        }).catch(function(error) {
+        }).catch(function (error) {
             document.getElementById("main-container").innerHTML = `<h1 class="text-bg-light">${error}</h1>`;
         });
     }
 
+    /**
+     * Validate date
+     * @param dateString = date to check if valid
+     * @returns {boolean} = true if date is valid, false if date isn't valid
+     */
     function isValidDate(dateString) {
         const date = new Date(dateString);
         return !isNaN(date.valueOf());
     }
 
+    /**
+     * Get data of published comments - according to ID media (= the ID is the date)
+     */
     function getDataPostedComments() {
         // Validation: Make sure the date object is defined and has a .date property
         if (!date.getNewDate().date || !isValidDate(date.getNewDate().date))
             document.getElementById("main-container").innerHTML = `<h1 class="text-bg-light">Error: date object is not defined or does not have a .date property</h1>`;
 
         fetch(`/resources/${date.getNewDate().date}`)
-            .then(function(response) {
-                if(response.status === 200)
+            .then(function (response) {
+                if (response.status === 200)
                     return response.json();
                 else
                     throw new Error(`code: ${response.code} Msg: ${response.msg}`);
-            }).then(function(data) {
+            }).then(function (data) {
             document.getElementById("comments-data").innerHTML = addCommentsToModal(data);
             document.querySelectorAll('.to-del').forEach((elem) => {
                 elem.addEventListener("click", delPost);
             });
-        }).catch(function(error) {
+        }).catch(function (error) {
             document.getElementById("main-container").innerHTML = `<h1>${error}</h1>`;
         });
     }
 
-    const addCommentsToModal = (data) =>{
+    /**
+     * Modify the submitted comment and add it
+     * @param data = Data of submitted comment
+     * @returns {string} string HTML of submitted comment
+     */
+    const addCommentsToModal = (data) => {
         let commentInfo = ``;
-        data.forEach(function(item) {
+        data.forEach(function (item) {
             let delLink = item.username === document.getElementById("uname").value ?
-                ` <a class="btn btn-link mr-2 to-del" href="del/${item.postId}" id="del-${item.postId}"> del </a>` : ``
+                ` <a class="btn btn-link mr-2 to-del" href="del/${item.postId}" id="del-${item.postId}">delete</a>` : ``
             commentInfo +=
                 `<div class="card mb-4">
                         <div class="card-body">
@@ -339,9 +397,14 @@
         return commentInfo;
     }
 
+    /**
+     * Delete wanted comment, only the writer has the option to delete his own comment
+     * The delete operation will display immediately to the writer
+     * @param event = delete button clicked
+     */
     function delPost(event) {
         event.preventDefault();
-        clearInterval(intrevalId);
+        clearInterval(intrevalId);              // Initialize 'intrevalId' timer
         getDataPostedComments()
         intrevalId = setInterval(getDataPostedComments, 15000);
         const deleteItem = event.target.id.replace("del-", "");
@@ -351,29 +414,38 @@
             return;
         }
         fetch(`/del/${deleteItem}`, {method: 'DELETE'})
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
-            }).then(function(data) {
-            if(data.status >= 400)
+            }).then(function (data) {
+            if (data.status >= 400)
                 throw new Error(`Msg: ${data.msg}`);
         })
-            .catch(function(error) {
+            .catch(function (error) {
                 document.getElementById("main-container").innerHTML = `<h1 class="text-bg-light">${error}</h1>`;
             });
     }
 
+    /**
+     * Validate entered username - must contain digits and letters only, length must be between 1-24.
+     * @returns {boolean} True if username is valid, False if not valid
+     */
     function validatorName() {
         const inpUname = document.getElementById("uname");
-        if (!(/^[A-Za-z0-9]{1,10}$/.test(inpUname.value.trim()))) {
+        if (!(/^[A-Za-z0-9]{1,24}$/.test(inpUname.value.trim()))) {
             inpUname.setCustomValidity("Please match the requested format.");
             return false;
-        }
-        else {
+        } else {
             inpUname.setCustomValidity("");
-            return true
+            return true;
         }
     }
-    const validatorInput = (input) =>{
+
+    /**
+     * Validate comment input - length must be between 1-128.
+     * @param input
+     * @returns {boolean}
+     */
+    const validatorInput = (input) => {
         input.preventDefault();
         if (!(input.target.value.trim().length > 0 && input.target.value.trim().length < 128)) {
             document.getElementById("inputSizeError").classList.remove('d-none');
@@ -382,7 +454,13 @@
         document.getElementById("inputSizeError").classList.add('d-none');
         return true;
     }
-    const fixDate = (date) =>{
+
+    /**
+     * Format date to mm/dd/yyyy
+     * @param date = date
+     * @returns {`${*}-${*}-${*}`}  = format of mm/dd/yyyy
+     */
+    const fixDate = (date) => {
         let dateComponents = (date).split("-");
         return `${dateComponents[1]}-${dateComponents[2]}-${dateComponents[0]}`;
     }
